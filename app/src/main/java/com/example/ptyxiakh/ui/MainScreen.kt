da76b445f88d2f83eb1bfe2817d4f0c7c14adc4e
dimitrisKlokidis <dimitrisklokidis@gmail.com>
2024-12-22 18:44:40 +0200

    ui improve

--- app/src/main/java/com/example/ptyxiakh/ui/MainScreen.kt
+++ app/src/main/java/com/example/ptyxiakh/ui/MainScreen.kt
@@ -1,5 +1,8 @@
 package com.example.ptyxiakh.ui
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -12,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -22,6 +26,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
@@ -53,159 +58,215 @@ import com.example.ptyxiakh.UiState
 fun MainScreen(
     geminiViewModel: GeminiViewModel = viewModel()
 ) {
-    val placeholderPrompt = stringResource(R.string.prompt_placeholder)
     val placeholderResult = stringResource(R.string.results_placeholder)
-    var prompt by rememberSaveable { mutableStateOf(placeholderPrompt) }
-    var result by rememberSaveable { mutableStateOf(placeholderResult) }
+    val result by rememberSaveable { mutableStateOf(placeholderResult) }
     val uiState by geminiViewModel.uiState.collectAsState()
 
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        Row(
-            modifier = Modifier.wrapContentSize()
-        ) {
-            IconButton(
-                onClick = { },
-                modifier = Modifier
-                    .padding(start = 5.dp)
-            ) {
-                Icon(
-                    modifier = Modifier
-                        .size(27.dp),
-                    imageVector = ImageVector.vectorResource(R.drawable.menu_24px),
-                    contentDescription = "",
-                )
-            }
-            Spacer(modifier = Modifier.weight(1f))
-            Row {
-                IconButton(
-                    onClick = { },
-                    modifier = Modifier
-                        .padding(end = 5.dp)
-                ) {
-                    Icon(
-                        modifier = Modifier
-                            .size(27.dp),
-                        imageVector = ImageVector.vectorResource(R.drawable.settings_24px),
-                        contentDescription = "",
-                    )
-                }
-            }
-        }
-        OutlinedTextField(
-            value = " ",
-            singleLine = false,
-            enabled = true,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(250.dp)
-                .padding(start = 10.dp, end = 10.dp, bottom = 10.dp),
-            onValueChange = {},
-            label = { Text("") },
-            textStyle = TextStyle(
-                textAlign = TextAlign.Start,
-                //fontFamily = FontFamily(Font(R.font.radiocanadabigregular)),
-                fontSize = 26.sp
-            ),
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedContainerColor = Color.Transparent,
-                unfocusedContainerColor = Color.Transparent,
-                focusedBorderColor = Color.Black, // Set the focused outline to black
-                unfocusedBorderColor = Color.Black,
-            ),
-            shape = RoundedCornerShape(20.dp)
+        TopButtons()
+        SpeechToTextUi()
+        ResultsUi(
+            uiState,
+            result,
+            Modifier.Companion.align(Alignment.CenterHorizontally),
+            Modifier.Companion.weight(1f),
+            Modifier.Companion
+                .align(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, end = 16.dp)
+                .weight(1f)
         )
-        if (uiState is UiState.Loading) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
-            Spacer(modifier = Modifier.weight(1f))
-        } else {
-            var textColor = MaterialTheme.colorScheme.onSurface
-            if (uiState is UiState.Error) {
-                textColor = MaterialTheme.colorScheme.error
-                result = (uiState as UiState.Error).errorMessage
-            } else if (uiState is UiState.Success) {
-                textColor = MaterialTheme.colorScheme.onSurface
-                result = (uiState as UiState.Success).outputText
-            }
-            val scrollState = rememberScrollState()
-            Text(
-                text = result,
-                textAlign = TextAlign.Start,
-                color = textColor,
-                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 20.sp),
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(start = 16.dp, end = 16.dp)
-                    .weight(1f)
-                    .verticalScroll(scrollState)
-            )
-        }
 
     }
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.BottomCenter
     ) {
-        Column {
-            Row(
-                modifier = Modifier.padding(end = 16.dp, start = 5.dp)
-            ) {
-                Spacer(modifier = Modifier.weight(1f))
-                Button(
-                    onClick = {
-                        geminiViewModel.sendPrompt(prompt)
-                    },
-                    enabled = prompt.isNotEmpty(),
-                    modifier = Modifier
-                        .size(56.dp)
-                        .clip(CircleShape), // Make it circular
-                    shape = CircleShape, // Ensure the button's shape is circular
-                    contentPadding = PaddingValues(0.dp) //remove extra padding
-                ) {
-                    Text(text = stringResource(R.string.ai))
-                }
-            }
-            Row(
-                modifier = Modifier
-                    .padding(start = 16.dp, end = 16.dp, bottom = 10.dp, top = 5.dp)
-                    .fillMaxWidth()
-                    .height(IntrinsicSize.Min) // Ensures both children match their heights
-            ) {
-                OutlinedTextField(
-                    value = prompt,
-                    label = { Text("") },
-                    onValueChange = { prompt = it },
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight(), // Makes the TextField fill the parent's height
-                    shape = RoundedCornerShape(50.dp),
-                    maxLines = 3,
-                    colors = OutlinedTextFieldDefaults.colors(
-                        focusedContainerColor = Color.White,
-                        unfocusedContainerColor = Color.White,
-                        focusedBorderColor = Color.Black,
-                        unfocusedBorderColor = Color.Black,
-                    ),
-                    trailingIcon = {
-                        Button(
-                            onClick = {
+        BottomUi(geminiViewModel::sendPrompt)
+    }
+}
 
-                            },
-                            modifier = Modifier.fillMaxHeight(),
-                            shape = RoundedCornerShape(
-                                topStart = 0.dp,
-                                bottomStart = 0.dp,
-                                topEnd = 50.dp,
-                                bottomEnd = 50.dp
-                            ),
-                            enabled = prompt.isNotEmpty()
-                        ) {
-                            Text(text = stringResource(R.string.action_go))
-                        }
-                    }
+@Composable
+private fun ResultsUi(
+    uiState: UiState,
+    result: String,
+    modifier: Modifier,
+    weightModifier: Modifier,
+    textModifier: Modifier
+) {
+    var result1 = result
+    if (uiState is UiState.Loading) {
+        CircularProgressIndicator(modifier = modifier)
+        Spacer(modifier = weightModifier)
+    } else {
+        var textColor = MaterialTheme.colorScheme.onSurface
+        if (uiState is UiState.Error) {
+            textColor = MaterialTheme.colorScheme.error
+            result1 = uiState.errorMessage
+        } else if (uiState is UiState.Success) {
+            textColor = MaterialTheme.colorScheme.onSurface
+            result1 = uiState.outputText
+        }
+        val scrollState = rememberScrollState()
+        Text(
+            text = result1,
+            textAlign = TextAlign.Start,
+            color = textColor,
+            style = MaterialTheme.typography.bodyLarge.copy(fontSize = 20.sp),
+            modifier = textModifier
+                .verticalScroll(scrollState)
+        )
+    }
+}
+
+@Composable
+private fun SpeechToTextUi() {
+    OutlinedTextField(
+        value = " ",
+        singleLine = false,
+        enabled = true,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(250.dp)
+            .padding(start = 10.dp, end = 10.dp, bottom = 10.dp),
+        onValueChange = {},
+        label = { Text("") },
+        textStyle = TextStyle(
+            textAlign = TextAlign.Start,
+            //fontFamily = FontFamily(Font(R.font.radiocanadabigregular)),
+            fontSize = 26.sp
+        ),
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedContainerColor = Color.Transparent,
+            unfocusedContainerColor = Color.Transparent,
+            focusedBorderColor = Color.Black, // Set the focused outline to black
+            unfocusedBorderColor = Color.Black,
+        ),
+        shape = RoundedCornerShape(20.dp)
+    )
+}
+
+@Composable
+private fun TopButtons() {
+    Row(
+        modifier = Modifier.wrapContentSize()
+    ) {
+        IconButton(
+            onClick = { },
+            modifier = Modifier
+                .padding(start = 5.dp)
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(27.dp),
+                imageVector = ImageVector.vectorResource(R.drawable.menu_24px),
+                contentDescription = "",
+            )
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Row {
+            IconButton(
+                onClick = { },
+                modifier = Modifier
+                    .padding(end = 5.dp)
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(27.dp),
+                    imageVector = ImageVector.vectorResource(R.drawable.settings_24px),
+                    contentDescription = "",
                 )
             }
+        }
+    }
+}
+
+@Composable
+private fun BottomUi(
+    sendPrompt: (String) -> Unit,
+) {
+    var prompt by rememberSaveable { mutableStateOf("") }
+    Column {
+        Row(
+            modifier = Modifier.padding(end = 16.dp, start = 5.dp)
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                onClick = {
+                    sendPrompt(prompt)
+                },
+                enabled = prompt.isNotEmpty(),
+                modifier = Modifier
+                    .size(56.dp)
+                    .clip(CircleShape), // Make it circular
+                shape = CircleShape, // Ensure the button's shape is circular
+                contentPadding = PaddingValues(0.dp) //remove extra padding
+            ) {
+                Text(text = stringResource(R.string.ai))
+            }
+        }
+        Row(
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, bottom = 10.dp, top = 5.dp)
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min) // Ensures both children match their heights
+        ) {
+            OutlinedTextField(
+                value = prompt,
+                label = { Text("") },
+                onValueChange = { prompt = it },
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight(), // Makes the TextField fill the parent's height
+                shape = RoundedCornerShape(50.dp),
+                maxLines = 3,
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White,
+                    focusedBorderColor = Color.Black,
+                    unfocusedBorderColor = Color.Black,
+                ),
+                trailingIcon = {
+                    OutlinedButton(
+                        onClick = {
+
+                        },
+                        modifier = Modifier
+                            .fillMaxHeight(),
+                        shape = RoundedCornerShape(
+                            topStart = 0.dp,
+                            bottomStart = 0.dp,
+                            topEnd = 50.dp,
+                            bottomEnd = 50.dp
+                        ),
+                        enabled = prompt.isNotEmpty(),
+                        border = BorderStroke(
+                            0.dp,
+                            Color.Transparent
+                        ), // Makes the outline transparent
+                        contentPadding = PaddingValues(0.dp) //remove extra padding
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(8.dp) // Space between line and icon
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .width(1.dp) // Line thickness
+                                    .fillMaxHeight() // Line height
+                                    .background(Color.Black) // Line color
+                            )
+                            Icon(
+                                modifier = Modifier
+                                    .size(30.dp),
+                                imageVector = ImageVector.vectorResource(R.drawable.graphic_eq_24px),
+                                contentDescription = "",
+                            )
+                        }
+                    }
+                }
+            )
         }
     }
 }
