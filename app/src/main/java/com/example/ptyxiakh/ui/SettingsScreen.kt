912cf2adb4f526a3f1b68ff4042890f76d3eb24d
dimitrisKlokidis <dimitrisklokidis@gmail.com>
2024-12-27 17:40:29 +0200

    new SettingsScreen

---
+++ app/src/main/java/com/example/ptyxiakh/ui/SettingsScreen.kt
@@ -0,0 +1,78 @@
+package com.example.ptyxiakh.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ptyxiakh.R
+
+@Composable
+fun SettingsScreen(){
+
+    val scrollState = rememberScrollState()
+
+    Box(
+        modifier = Modifier,
+        contentAlignment = Alignment.TopStart
+    ) {
+        IconButton(
+            onClick = {  },
+            modifier = Modifier
+                .padding(start = 5.dp)
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack, // Use the appropriate icon
+                contentDescription = stringResource(id = R.string.back),
+                modifier = Modifier.size(30.dp)
+            )
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = stringResource(R.string.settings),
+                style = MaterialTheme.typography.titleSmall.copy(fontSize = 35.sp)
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(scrollState)
+        ) {
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SettingsScreenPreview() {
+    SettingsScreen()
+}
