028d3c49afa7668270ea02206e8140f5b529fdbb
dimitrisKlokidis <dimitrisklokidis@gmail.com>
2025-03-11 18:10:35 +0200

    first version SoundDetectionViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
+
+    //tensorflow
+    implementation(libs.tensorflow.lite)
 }
