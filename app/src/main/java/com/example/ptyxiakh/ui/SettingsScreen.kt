48c91a46e89a62390e8a6e2c8901d6d7652663ef
dimitrisKlokidis <dimitrisklokidis@gmail.com>
2025-03-30 19:00:08 +0300

    add settings

--- app/src/main/java/com/example/ptyxiakh/ui/SettingsScreen.kt
+++ app/src/main/java/com/example/ptyxiakh/ui/SettingsScreen.kt
@@ -8,12 +8,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.Help
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
@@ -21,6 +24,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -30,6 +34,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -83,6 +88,7 @@ fun SettingsScreen(
                 .fillMaxSize()
                 .verticalScroll(scrollState)
         ) {
+            Spacer(modifier = Modifier.padding(5.dp))
             SettingSwitch(
                 text = stringResource(R.string.vibrate),
                 onClick = dataStorePrefViewModel::toggleVibration,
@@ -92,11 +98,67 @@ fun SettingsScreen(
             SettingSwitchWithExplain(
                 text = stringResource(R.string.auto_mic),
                 onClick = dataStorePrefViewModel::toggleAutoMic,
-                value = uiState.autoMic
+                value = uiState.autoMic,
+                titleText = stringResource(R.string.auto_mic),
+                bodyText = stringResource(R.string.auto_mic_meaning)
             )
             Spacer(modifier = Modifier.padding(5.dp))
-            OneSettingSimple(stringResource(R.string.edit_user_data),navigateUserDetails)
+            OneSettingSimple(stringResource(R.string.edit_user_data), navigateUserDetails)
+            Spacer(modifier = Modifier.padding(5.dp))
+            OneSettingSimpleDialog(
+                text = stringResource(R.string.tts_language_title),
+                titleText = stringResource(R.string.tts_language_title),
+                bodyText = stringResource(R.string.tts_language_description),
+            )
         }
+    }
+}
+
+@Composable
+fun OneSettingSimpleDialog(
+    text: String,
+    titleText: String,
+    bodyText: String
+) {
+    var showDialog by remember { mutableStateOf(false) }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { showDialog = true }
+            .size(60.dp)
+            .padding(start = 15.dp, end = 15.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleSmall.copy(fontSize = 25.sp)
+        )
+    }
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text(titleText) },
+            text = {
+                Box(
+                    modifier = Modifier
+                        .heightIn(max = 300.dp) // Limits height to enable scrolling
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    Text(bodyText)
+                }
+            },
+            confirmButton = {
+                TextButton(onClick = { showDialog = false }) {
+                    Text(
+                        stringResource(R.string.ok),
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+                }
+            },
+        )
     }
 }
 
@@ -144,7 +206,15 @@ fun SettingSwitch(text: String, onClick: (Boolean) -> Unit, value: Boolean) {
 }
 
 @Composable
-fun SettingSwitchWithExplain(text: String, onClick: (Boolean) -> Unit, value: Boolean) {
+fun SettingSwitchWithExplain(
+    text: String,
+    onClick: (Boolean) -> Unit,
+    value: Boolean,
+    titleText: String,
+    bodyText: String
+) {
+    var showDialog by remember { mutableStateOf(false) }
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -158,11 +228,43 @@ fun SettingSwitchWithExplain(text: String, onClick: (Boolean) -> Unit, value: Bo
             text = text,
             style = MaterialTheme.typography.titleSmall.copy(fontSize = 25.sp)
         )
+        Spacer(modifier = Modifier.padding(5.dp))
+        IconButton(
+            onClick = { showDialog = true },
+            modifier = Modifier.size(20.dp)
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.Help,
+                contentDescription = stringResource(R.string.explain),
+                modifier = Modifier.size(20.dp)
+            )
+        }
+        Spacer(modifier = Modifier.padding(5.dp))
         Spacer(modifier = Modifier.weight(1f))
         Switch(
             checked = value,
             onCheckedChange = {
                 onClick(!value)
+            }
+        )
+    }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text(titleText) },
+            text = {
+                Text(bodyText)
+            },
+            confirmButton = {
+                TextButton(onClick = { showDialog = false }) {
+                    Text(
+                        stringResource(R.string.ok),
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+                }
             }
         )
     }
