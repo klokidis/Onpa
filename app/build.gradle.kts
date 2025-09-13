8c24b302f33839c5c6894243f81e82a387675a71
dimitrisklokidis <dimitrisklokidis@gmail.com>
2025-09-13 17:58:41 +0300

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.yukuro.onpa"
         minSdk = 27
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
